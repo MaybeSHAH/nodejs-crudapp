c1edbde734a4b9fd1b386e2417a63ab92e46c339
Liyakat Shaikh <liyakatshaikh07@gmail.com>
2020-12-25 14:31:14 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Item = require('./models/items');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+const mongodb = "mongodb+srv://ckmobile:Love@1435@cluster0.4zxzv.mongodb.net/<dbname>?retryWrites=true&w=majority"
+mongoose.connect(mongodb,{useNewUrlParser: true, useUnifiedTopology: true} ).then(()=>console.log('connected')).catch(err=> console.log(err));
+app.set('view engine', 'ejs');
+app.listen(3000);
+
+
+app.get("/", (req, res)=>{
+    res.redirect('/get-items');
+})
+app.get("/get-items", (req, res)=>{
+    
+    Item.find().then(result=> {
+        res.render('index', {items: result })
+    }).catch(err=> 
+        console.log(err));
+})
+app.get("/add-item", (req, res) =>{
+    res.render('add-item');
+})
+app.post("/items",(req, res)=>{
+    console.log(req.body);
+    const item = Item(req.body);
+    item.save().then(()=> {
+        res.redirect('/get-items')
+    }).catch(err=> console.log(err))
+})
+app.get('/items/:id', (req, res)=>{
+    const id = req.params.id;
+    Item.findById(id).then(result=>{
+        res.render('item-detail',{item: result})
+    })
+})
+app.delete('/items/:id', (req, res)=>{
+    const id = req.params.id;
+    Item.findByIdAndDelete(id).then(result=>{
+        res.json({ redirect: '/get-items'})
+    })
+})
+app.put('/items/:id', (req, res)=>{
+    const id = req.params.id;
+    Item.findByIdAndUpdate(id, req.body).then(result=>{
+        res.json({ msg: 'successfully updated'})
+    })
+})
+app.use( (req, res) =>{
+    res.render('error');
+})
